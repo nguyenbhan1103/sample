fe4893aaa4f63fbc1cbe314457a7245b009834e2
an <newuser@example.com>
2016-12-09 10:11:23 +0700

    chu thich cho lan commit nay

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-  	  if logged_in?
+  	if logged_in?
+      @comment  = current_user.comments.build
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      @feed_items = Micropost.all.paginate(page: params[:page], :per_page => 5)
     end
-
+    
   end
 
   def help
